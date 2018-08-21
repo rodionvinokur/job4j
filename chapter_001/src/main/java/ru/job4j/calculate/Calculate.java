d6ee9b7fbe87513e9014126c60c4045871666f02
rodionvinokur <mailto:slevin@live.ru>
2018-08-21 22:01:33 +0300

    1.3. JavDoc. Code convetion.[#67495]

--- chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
@@ -1,7 +1,18 @@
 package ru.job4j.calculate;
-
+/**
+* Calculate
+* @author Rodion V.
+* @version 1.0
+* @since 1.0
+*/
 public class Calculate {
 
+/**
+* out to console "Hello world!"
+* @version 1.0
+* @param String[]
+* @return void
+*/
   public static void main(String[] args) {
     System.out.println("Hello world!");
   }
---
+++ chapter_001/src/main/java/ru/job4j/calculate/package-info.java
@@ -0,0 +1,8 @@
+/**
+* Package for Calculate task
+*
+* @author Rodion V.
+* @version 1.0
+* @since 1.0
+*/
+package ru.job4j.calculate;
