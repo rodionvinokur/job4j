58e1578f486141ba6e1f667992efffbf22905c27
rodionvinokur <mailto:slevin@live.ru>
2018-08-24 13:30:34 +0300

    6.0. Заполнить массив степенями чисел. [#33488]

--- chapter_001/src/test/java/ru/job4j/array/SquareTest.java
+++ chapter_001/src/test/java/ru/job4j/array/SquareTest.java
@@ -13,7 +13,7 @@ import static org.junit.Assert.assertThat;
 public class SquareTest {
 
     /**
-     * Test for Method calculate(int);
+     * Test for Method calculate(bound - 3).
      */
     @Test
     public void whenBound3Then149() {
@@ -21,6 +21,40 @@ public class SquareTest {
         Square square = new Square();
         int[] rst = square.calculate(bound);
         int[] expect = new int[]{1, 4, 9};
+        assertThat(rst, is(expect));
+    }
+
+    /**
+     * Test for Method calculate(bound - 4).
+     */
+    @Test
+    public void whenBound4Then14916() {
+        int bound = 4;
+        Square square = new Square();
+        int[] rst = square.calculate(bound);
+        int[] expect = new int[]{1, 4, 9, 16};
+        assertThat(rst, is(expect));
+    }
+    /**
+     * Test for Method calculate(bound - 5).
+     */
+    @Test
+    public void whenBound4Then1491625() {
+        int bound = 5;
+        Square square = new Square();
+        int[] rst = square.calculate(bound);
+        int[] expect = new int[]{1, 4, 9, 16, 25};
+        assertThat(rst, is(expect));
+    }
+    /**
+     * Test for Method calculate(bound - 0).
+     */
+    @Test
+    public void whenBound0Then() {
+        int bound = 0;
+        Square square = new Square();
+        int[] rst = square.calculate(bound);
+        int[] expect = new int[]{};
         assertThat(rst, is(expect));
     }
 }
