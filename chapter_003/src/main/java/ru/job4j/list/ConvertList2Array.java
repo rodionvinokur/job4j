9a457ad911d7a4dee82d2fbba14a59b4805c8316
rodionvinokur <mailto:slevin@live.ru>
2018-10-15 14:40:31 +0300

    Конвертация ArrayList в двухмерный массив

--- chapter_003/src/main/java/ru/job4j/list/ConvertList2Array.java
+++ chapter_003/src/main/java/ru/job4j/list/ConvertList2Array.java
@@ -12,21 +12,18 @@ import java.util.stream.IntStream;
  * @since 1.0
  */
 public class ConvertList2Array {
-       public int[][] toArray(List<Integer> lst, int rows) {
+    public int[][] toArray(List<Integer> lst, int rows) {
         if (!lst.isEmpty() && lst.size() >= rows) {
-            List<Integer> list = new ArrayList<>(lst);
-            int columns = lst.size() / rows + (list.size() % rows > 0 ? 1 : 0);
-            IntStream.range(0, rows * columns - lst.size()).forEach(x -> list.add(0));
-            int i = 0;
-            int index = 0;
-            int[][] array = new int[rows][];
-            for (int[] element : array) {
-                array[i] = list.subList(index, index + columns)
-                        .stream()
-                        .mapToInt(Integer::intValue)
-                        .toArray();
-                index += columns;
-                i++;
+            int columns = lst.size() / rows + (lst.size() % rows > 0 ? 1 : 0);
+            int[][] array = new int[rows][columns];
+            int indexRow = 0;
+            int indexColumn = 0;
+            for (Integer element : lst) {
+                array[indexRow][indexColumn++] = element;
+                if (indexColumn == columns) {
+                    indexColumn = 0;
+                    indexRow++;
+                }
             }
             return array;
         }
--- chapter_003/src/test/java/ru/job4j/list/ConvertList2ArrayTest.java
+++ chapter_003/src/test/java/ru/job4j/list/ConvertList2ArrayTest.java
@@ -1,7 +1,9 @@
 package ru.job4j.list;
 
 import org.junit.Test;
+
 import java.util.Arrays;
+
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertThat;
 
@@ -14,7 +16,7 @@ import static org.junit.Assert.assertThat;
  */
 public class ConvertList2ArrayTest {
     @Test
-    public void when7ElementsThen9() {
+    public void when7ElementsThen9Row3() {
         ConvertList2Array list = new ConvertList2Array();
         int[][] result = list.toArray(Arrays.asList(1, 2, 3, 4, 5, 6, 7), 3);
         int[][] expect = {
@@ -24,4 +26,30 @@ public class ConvertList2ArrayTest {
         };
         assertThat(result, is(expect));
     }
+
+    @Test
+    public void when7ElementsThen9Row2() {
+        ConvertList2Array list = new ConvertList2Array();
+        int[][] result = list.toArray(Arrays.asList(1, 2, 3, 4, 5, 6, 7), 2);
+        int[][] expect = {
+                {1, 2, 3, 4},
+                {5, 6, 7, 0}
+        };
+        assertThat(result, is(expect));
+    }
+
+    @Test
+    public void when7ElementsThen9Row4() {
+        ConvertList2Array list = new ConvertList2Array();
+        int[][] result = list.toArray(Arrays.asList(1, 2, 3, 4, 5, 6, 7), 4);
+        int[][] expect = {
+                {1, 2},
+                {3, 4},
+                {5, 6},
+                {7, 0}
+        };
+        assertThat(result, is(expect));
+    }
+
+
 }
