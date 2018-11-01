a39541f0fbe53d803815397cf736d407bf181420
rodionvinokur <mailto:slevin@live.ru>
2018-11-01 23:42:04 +0300

    5.3.4. Задан связанный список. Определить цикличность.[#79042]

--- chapter_005_000/src/main/java/ru/job4j/list/LinkList.java
+++ chapter_005_000/src/main/java/ru/job4j/list/LinkList.java
@@ -20,67 +20,23 @@ public class LinkList {
         Node<T> next;
     }
 
-    /**
-     * метод hasCycle для поиска цикличности в списках переменной глубины
-     * Глубина по условию не известна. Метод в цикле начинает подбирать глубину
-     * начиная с 1 (int sizeList = 1)
-     * По мере анализа, если не выевляется цикличность или не цикличность глубина увеличивается
-     * (sizeList++;)
-     * Анализ производиться в процедуре doTest(...), в нее передается стартовый нод и размер списка
-     * возврат doTest - NO - нет цикличности, YES - есть, YET - продолжить анализ
-     *
-     * @param startNode - начальный год списка
-     * @return
-     */
     public boolean hasCycle(Node startNode) {
-        int sizeList = 1;
-        while (true) {
-            int result = doTest(startNode, sizeList);
-            if (result == NO) {
-                return false;
-            }
-            if (result == YES) {
-                return true;
-            }
-            sizeList++;
+        if (step(startNode) == null) {
+            return false;
         }
+        Node cherepaha = step(startNode);
+        Node zayac = step(cherepaha);
+        while (cherepaha != zayac && zayac != null) {
+            cherepaha = step(cherepaha);
+            zayac = step(step(zayac));
+        }
+        return zayac != null;
     }
-
-    /**
-     * doTest анализ цикличности списка фиксированного размера
-     * (currentNode == null) - на случай если первый элемент списка NULL
-     * (currentNode == currentNode.next) - предварительный анализ на цикличность
-     * Цикл for (j = sizeList; j > 1; j--) задает смещение оносительно начала списка для
-     * выявления замыканий кольцом вглубине списка.
-     * for (i = sizeList; i > 1; i--) - элементарная операция для списка фиксированной
-     * длины для выявления замыканий типа от A -> . -> . -> A.
-     *
-     * @param currentNode
-     * @param sizeList - начальный размер
-     * @return
-     */
-    int doTest(Node currentNode, int sizeList) {
-        int i = sizeList;
-        int j = sizeList;
-        if (currentNode == null) {
-            return NO;
-        }
-        if (currentNode == currentNode.next) {
-            return YES;
-        }
-        for (j = sizeList; j > 1; j--) {
-            Node checkNode = currentNode;
-            for (i = sizeList; i > 1; i--) {
-                checkNode = checkNode.next;
-                if (currentNode == checkNode) {
-                    return YES;
-                }
-                if (checkNode == null) {
-                    return NO;
-                }
-            }
-            currentNode = currentNode.next;
-        }
-        return YET;
+    private Node step(Node node) {
+        return (node == null)
+                ? null
+                : (node.next == null)
+                    ? null
+                    : node.next;
     }
 }
