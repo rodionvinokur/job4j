e780693d1a322595c30537d953f4b7192cb7e0a7
rodionvinokur <mailto:slevin@live.ru>
2018-11-04 18:21:54 +0300

    4. Переопределить только equals[#79060]

--- chapter_005_000/src/main/java/ru/job4j/map/User.java
+++ chapter_005_000/src/main/java/ru/job4j/map/User.java
@@ -22,7 +22,15 @@ public class User {
     }
 
     @Override
-    public int hashCode() {
-        return Objects.hash(name, children, birthday);
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        User user = (User) o;
+
+        if (children != user.children) return false;
+        if (name != null ? !name.equals(user.name) : user.name != null) return false;
+        return birthday != null ? birthday.equals(user.birthday) : user.birthday == null;
     }
+
 }
