ecc61c088d370d85328d675e1ca48056d310995c
rodionvinokur <mailto:slevin@live.ru>
2018-11-04 17:51:57 +0300

    2. Не перекрывать equals hashCode[#79061]

---
+++ chapter_005_000/src/test/java/ru/job4j/map/UserTest.java
@@ -0,0 +1,30 @@
+package ru.job4j.map;
+
+import org.junit.Test;
+
+import java.util.GregorianCalendar;
+import java.util.HashMap;
+import java.util.Map;
+
+import static org.junit.Assert.*;
+
+/**
+ * UserTest
+ *
+ * @author Rodion V.
+ * @version 1.0
+ * @since 1.0
+ */
+public class UserTest {
+    @Test
+    public void testPrintMapUsers() {
+        Map<User, Object> mapUsrO = new HashMap<>();
+        User usr1 = new User("Petro", 1, new GregorianCalendar(2011, 1 , 25));
+        User usr2 = new User("Petro", 1, new GregorianCalendar(2011, 1 , 25));
+        String one = "one";
+        String two = "two";
+        mapUsrO.put(usr1, one);
+        mapUsrO.put(usr1, two);
+        System.out.println(mapUsrO);
+    }
+}
