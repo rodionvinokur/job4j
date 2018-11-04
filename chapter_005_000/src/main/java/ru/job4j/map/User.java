3a42c9f910e116eec5fc31117d5600d786963b54
rodionvinokur <mailto:slevin@live.ru>
2018-11-04 16:39:28 +0300

    1. Создать модель User[#79057]

---
+++ chapter_005_000/src/main/java/ru/job4j/map/User.java
@@ -0,0 +1,22 @@
+package ru.job4j.map;
+
+import java.util.Calendar;
+
+/**
+ * User
+ *
+ * @author Rodion V.
+ * @version 1.0
+ * @since 1.0
+ */
+public class User {
+    private String name;
+    private int children;
+    private Calendar birthday;
+
+    public User(String name, int children, Calendar birthday) {
+        this.name = name;
+        this.children = children;
+        this.birthday = birthday;
+    }
+}
